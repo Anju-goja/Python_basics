c4a7c6081e31f0bee16b386333eaed09a03abf4f
Anju-goja <anjugoja@gmail.com>
2018-06-26 16:51:37 +0530

    updating all files

---
+++ prime no.py
@@ -0,0 +1,4 @@
+n= int (input("enter any number"))
+n=5
+for(i=0;1<=5;i++)
+    
---
+++ test.py
@@ -0,0 +1,32 @@
+#wap to take two string and print them together using + operator
+
+f_name="sant"
+l_name="kumar"
+
+print(f_name+l_name)
+
+
+
+a="hi this is preeti\n"
+b="hi i am so cute"
+print (type(b))
+print (a + b)
+
+#wap to take two numbers and then display their sum
+
+
+a=10
+print (type(a))
+b=5
+print("the sum of a and b is ",(a+b))
+
+#print this is backslash
+
+print ("this is backslash","\\")
+
+#using place holders print My name is ____ and my age is ____ and marks are ____
+age=22
+name="pagal"
+marks=70.6
+print (type(marks))
+print (" my name  is %s and my age  is %d and my marks are%f" %(name,age,marks))
